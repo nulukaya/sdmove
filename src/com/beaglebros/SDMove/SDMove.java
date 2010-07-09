7a43801d9dc99e16fe9dd3eebc2c7d36c4f6a725
wfaulk <wfaulk@localhost>
2010-07-09 00:31:26 -0400

    cleanup move pkglist population to its own method sort list

--- src/com/beaglebros/SDMove/SDMove.java
+++ src/com/beaglebros/SDMove/SDMove.java
@@ -4,7 +4,6 @@ package com.beaglebros.SDMove;
 // for pointing me in the right direction for dealing with the apk's xml file
 // <https://www.isecpartners.com/manifest_explorer.html>
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
 import android.os.Bundle;
@@ -17,7 +16,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.content.Context;
 import android.content.Intent;
@@ -29,6 +27,8 @@ import android.graphics.Color;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -79,113 +79,18 @@ public class SDMove extends ListActivity {
 	private static final int internalOnly = 1;
 	private static final int preferExternal = 2;
 	
-	String tmp;
-
+	private static final int ABOUT_DIALOG = 0;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		final List<PkgListItem> pkglist = new ArrayList<PkgListItem>();        
-		PackageManager pm = getPackageManager();
+		final ArrayList<PkgListItem> pkglist = new ArrayList<PkgListItem>();        
 		
+		getPackages(pkglist);
 		
-		for (PackageInfo pkg: pm.getInstalledPackages(0)) {
-			String packageName;
-			packageName = pkg.packageName;
-			if ( packageName == null || packageName == "" ) {
-				packageName = "android";
-			}
-			
-			// Bad First Attempt
-			/*
-			text += pkg.applicationInfo.publicSourceDir;
-			JarFile apk;
-			try {
-				apk = new JarFile(pkg.applicationInfo.publicSourceDir);
-				InputStream apkIS = apk.getInputStream(apk.getEntry("AndroidManifest.xml"));
-				byte buf[];
-				buf = new byte[256];
-				String sbuf;
-				
-				while (apkIS.read(buf) != -1) {
-					sbuf = new String(buf);
-					text += sbuf;
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-			}
-			*/
-			
-			try {
-				AssetManager am = createPackageContext(packageName, 0).getAssets();
-				XmlResourceParser xml = am.openXmlResourceParser("AndroidManifest.xml");
-				try {
-					int eventType = xml.getEventType();
-					xmlloop:
-					while (eventType != XmlPullParser.END_DOCUMENT) {
-						switch (eventType) {
-							case XmlPullParser.START_TAG:
-								if (! xml.getName().matches("manifest")) {
-									break xmlloop;
-								} else {
-									attrloop:
-									for (int j = 0; j < xml.getAttributeCount(); j++) {
-										if (xml.getAttributeName(j).matches("installLocation")) {
-											//pkglist.add(pkg.applicationInfo.loadLabel(pm).toString());
-											switch (Integer.parseInt(xml.getAttributeValue(j))) {
-											case auto:
-												tmp = "May Be Moved\n";
-												if ((pkg.applicationInfo.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0) {
-													tmp += "(already moved)";
-													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.GREEN));
-												} else {
-													tmp += "(not already moved)";
-													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.YELLOW));
-												}
-												//pkginfolist.add(tmp);
-												break;
-											case internalOnly:
-												//pkginfolist.add("Cannot Be Moved");
-												pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), "Cannot Be Moved", Color.RED));
-												break;
-											case preferExternal:
-												tmp = "External Storage Preferred\n";
-												if ((pkg.applicationInfo.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0) {
-													tmp += "(already moved)";
-													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.BLUE));
-												} else {
-													tmp += "(not already moved)";
-													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.CYAN));
-												}
-												//pkginfolist.add(tmp);
-												break;
-											default:
-												//pkginfolist.add("Crazy Wackiness!");
-												pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), "Crazy Wackiness!", Color.MAGENTA));
-												break;
-											}
-											break attrloop;
-										}
-									}
-								}
-								break;
-						}
-						eventType = xml.nextToken();
-					}
-				} catch (IOException ioe) {
-					// TODO showError("Reading XML", ioe);
-				} catch (XmlPullParserException xppe) {
-					// TODO showError("Parsing XML", xppe);
-				}
-
-			} catch (NameNotFoundException e) {
-				// TODO Auto-generated catch block
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-			}
-
-		}
+		Collections.sort(pkglist, new byPkgName());
 	
 		setListAdapter(new PkgListItemAdapter(this, android.R.layout.simple_list_item_1, pkglist));
 		ListView lv = getListView();
@@ -202,32 +107,15 @@ public class SDMove extends ListActivity {
 				if (acts.size() > 0) {
 					startActivity(it);
 				}
-				
-				//Toast.makeText(getApplicationContext(), pkglist.get(position).meta, Toast.LENGTH_SHORT).show();
-
 			}
 		});
-
-		/*
-		try {
-			PackageInfo tmppkg = pm.getPackageInfo("com.android.providers.applications", PackageManager.GET_RECEIVERS);
-			for (ActivityInfo ai: tmppkg.receivers) {
-				Toast.makeText(getApplicationContext(), ai.name, Toast.LENGTH_LONG).show();
-			}
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			Toast.makeText(getApplicationContext(), (e1.getMessage()!=null)?e1.getMessage():e1.toString(), Toast.LENGTH_LONG).show();
-			e1.printStackTrace();
-		}
-		//Toast.makeText(getApplicationContext(), "thing", Toast.LENGTH_LONG);
-		*/
 
 	}
 	
 	protected Dialog onCreateDialog(int id, Bundle args) {
 		Dialog d = new Dialog(this);
 		switch (id) {
-		case 0:
+		case ABOUT_DIALOG:
 			d.setContentView(R.layout.aboutdialog);
 			d.setTitle("About");
 			break;
@@ -254,6 +142,90 @@ public class SDMove extends ListActivity {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+
+	private void getPackages(ArrayList<PkgListItem> pkglist) {
+		PackageManager pm = getPackageManager();
+		
+		String tmp;
+	
+		for (PackageInfo pkg: pm.getInstalledPackages(0)) {
+			String packageName;
+			packageName = pkg.packageName;
+			if ( packageName == null || packageName == "" ) {
+				packageName = "android";
+			}
+			
+			try {
+				AssetManager am = createPackageContext(packageName, 0).getAssets();
+				XmlResourceParser xml = am.openXmlResourceParser("AndroidManifest.xml");
+				try {
+					int eventType = xml.getEventType();
+					xmlloop:
+					while (eventType != XmlPullParser.END_DOCUMENT) {
+						switch (eventType) {
+							case XmlPullParser.START_TAG:
+								if (! xml.getName().matches("manifest")) {
+									break xmlloop;
+								} else {
+									attrloop:
+									for (int j = 0; j < xml.getAttributeCount(); j++) {
+										if (xml.getAttributeName(j).matches("installLocation")) {
+											switch (Integer.parseInt(xml.getAttributeValue(j))) {
+											case auto:
+												tmp = "May Be Moved\n";
+												if ((pkg.applicationInfo.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0) {
+													tmp += "(already moved)";
+													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.GREEN));
+												} else {
+													tmp += "(not already moved)";
+													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.YELLOW));
+												}
+												break;
+											case internalOnly:
+												pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), "Cannot Be Moved", Color.RED));
+												break;
+											case preferExternal:
+												tmp = "External Storage Preferred\n";
+												if ((pkg.applicationInfo.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0) {
+													tmp += "(already moved)";
+													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.BLUE));
+												} else {
+													tmp += "(not already moved)";
+													pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), tmp, Color.CYAN));
+												}
+												break;
+											default:
+												pkglist.add(new PkgListItem(pkg, pkg.applicationInfo.loadLabel(pm).toString(), "Crazy Wackiness!", Color.MAGENTA));
+												break;
+											}
+											break attrloop;
+										}
+									}
+								}
+								break;
+						}
+						eventType = xml.nextToken();
+					}
+				} catch (IOException ioe) {
+					// TODO showError("Reading XML", ioe);
+				} catch (XmlPullParserException xppe) {
+					// TODO showError("Parsing XML", xppe);
+				}
+	
+			} catch (NameNotFoundException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+	
+		}
+	}
+
+	private final class byPkgName implements Comparator<PkgListItem> {
+		public int compare(PkgListItem a, PkgListItem b) {
+		   return(a.toString()).compareToIgnoreCase(b.toString());
+	     }
 	}
 
 }
